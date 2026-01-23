86f321dab631ff1cb44ad44e3e2330e2694251f4
Uziel Flores <uzielo8017518@gmail.com>
2026-01-22 21:16:34 -0600

    Creacion de modelos SocialNetwork y SocialUser para la seccion de contacto

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableAdminMixin
+from .models import SocialNetwork, SocialUser
 
-# Register your models here.
+@admin.register(SocialNetwork)
+class SocialNetworkAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'modified')
+    list_display = ('id', 'name', 'base_url')
+    search_fields = ('name', 'slug')
+    date_hierarchy = 'created'
+
+@admin.register(SocialUser)
+class SocialUserAdmin(SortableAdminMixin, admin.ModelAdmin):
+    readonly_fields = ('created', 'modified')
+    list_display = ('id', 'username', 'social_network', 'profile_url', 'order')
+    list_filter = ('social_network',)
+    search_fields = ('username', 'social_network__name', 'use')
+    ordering = ('order',)
+    date_hierarchy = 'created'
+    sortable_by = ('order',)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class SocialNetwork(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Nombre')
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='Nombre en minúsculas y guión bajo')
+    base_url = models.URLField(max_length=100, verbose_name='URL base')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creado')
+    modified = models.DateTimeField(auto_now=True, verbose_name='Fecha de modificado')
+
+    class Meta:
+        verbose_name = 'Red Social'
+        verbose_name_plural = 'Redes Sociales'
+
+    def __str__(self):
+        return self.name
+    
+class SocialUser(models.Model):
+    social_network = models.ForeignKey(SocialNetwork, on_delete=models.PROTECT, verbose_name='Red Social')
+    username = models.CharField(max_length=50, verbose_name='Nombre de usuario')
+    profile_url = models.CharField(max_length=100, verbose_name='Path del perfil')
+    text = models.CharField(max_length=100, verbose_name='Texto')
+    use = models.CharField(max_length=50, verbose_name='Uso de la red social', null=True, blank=True)
+    order = models.PositiveSmallIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creado')
+    modified = models.DateTimeField(auto_now=True, verbose_name='Fecha de modificado')
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = 'Usuario de red social'
+        verbose_name_plural = 'Usuarios de redes sociales'
+
+    def __str__(self):
+        return f"{self.username} en {self.social_network.name}"
