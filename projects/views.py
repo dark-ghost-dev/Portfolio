694b2838e86aace671e49322d27b06ecef794498
Uziel Flores <uzielo8017518@gmail.com>
2026-02-03 11:47:47 -0600

    bugfix a la seccion de proyectos

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import redirect
 from core.models import SocialUser
 
 def projects(request):
-    social_users_in_hero = SocialUser.objects.filter(active=True, is_in_footer=True)
+    social_users_in_hero = SocialUser.objects.filter(active=True, is_in_hero=True)
     #return redirect('home')
-    return render(request, 'projects/project.html', {'social_users_in_footer': social_users_in_hero})
+    return render(request, 'projects/project.html', {'social_users_in_hero': social_users_in_hero})
