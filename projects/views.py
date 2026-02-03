db9928f77b41ccfa8cfc8737747804e71d4460f3
Uziel Flores <uzielo8017518@gmail.com>
2026-02-03 16:20:26 -0600

    Implementación de funcionalidad al template de proyectos

--- projects/views.py
+++ projects/views.py
@@ -4,11 +4,10 @@ from .models import Project
 from core.models import SocialUser
 
 def projects(request):
-    social_users_in_hero = SocialUser.objects.filter(active=True, is_in_hero=True)
-    #return redirect('home')
-    return render(request, 'projects/project.html', {'social_users_in_hero': social_users_in_hero})
+    return redirect('home')
 
 def project(request, project_slug):
-    social_users_in_hero = SocialUser.objects.filter(active=True, is_in_hero=True)
     project = get_object_or_404(Project, slug=project_slug, active=True)
-    return render(request, 'projects/project.html', {'project': project, 'social_users_in_hero': social_users_in_hero})
+    projects = Project.objects.filter(active=True)
+    social_users_in_hero = SocialUser.objects.filter(active=True, is_in_hero=True)
+    return render(request, 'projects/project.html', {'project': project, 'projects': projects, 'social_users_in_hero': social_users_in_hero})
